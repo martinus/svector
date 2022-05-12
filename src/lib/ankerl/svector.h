355fb31ac82c7127a7a8fbd2135227cb32494603
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2022-05-12 19:28:10 +0200

    CamelCase -> snake_case

--- src/lib/ankerl/svector.h
+++ src/lib/ankerl/svector.h
@@ -11,12 +11,12 @@ namespace ankerl {
 
 namespace detail {
 
-class SmallVectorHeader {
+class header {
     size_t m_size{};
     size_t const m_capacity;
 
 public:
-    inline explicit SmallVectorHeader(size_t capacity)
+    inline explicit header(size_t capacity)
         : m_capacity{capacity} {}
 
     [[nodiscard]] inline auto size() const -> size_t {
@@ -33,28 +33,28 @@ public:
 };
 
 template <typename T>
-struct SmallVectorStorage : public SmallVectorHeader {
-    static constexpr auto alignmentOfT = std::alignment_of_v<T>;
-    static constexpr auto maxAlignment = std::max(std::alignment_of_v<SmallVectorHeader>, std::alignment_of_v<T>);
-    static constexpr auto offsetToData = ((sizeof(SmallVectorHeader) + alignmentOfT - 1) / alignmentOfT) * alignmentOfT;
-    static_assert(maxAlignment <= __STDCPP_DEFAULT_NEW_ALIGNMENT__);
+struct storage : public header {
+    static constexpr auto alignment_of_t = std::alignment_of_v<T>;
+    static constexpr auto max_Alignment = std::max(std::alignment_of_v<header>, std::alignment_of_v<T>);
+    static constexpr auto offset_to_data = ((sizeof(header) + alignment_of_t - 1) / alignment_of_t) * alignment_of_t;
+    static_assert(max_Alignment <= __STDCPP_DEFAULT_NEW_ALIGNMENT__);
 
-    explicit SmallVectorStorage(size_t capacity)
-        : SmallVectorHeader(capacity) {}
+    explicit storage(size_t capacity)
+        : header(capacity) {}
 
     auto data() -> T* {
-        auto ptrToData = reinterpret_cast<std::byte*>(this) + offsetToData;
-        return reinterpret_cast<T*>(ptrToData);
+        auto ptr_to_data = reinterpret_cast<std::byte*>(this) + offset_to_data;
+        return reinterpret_cast<T*>(ptr_to_data);
     }
 
     auto data() const -> T const* {
-        auto ptrToData = reinterpret_cast<std::byte*>(this) + offsetToData;
-        return reinterpret_cast<T*>(ptrToData);
+        auto ptr_to_data = reinterpret_cast<std::byte const*>(this) + offset_to_data;
+        return reinterpret_cast<T const*>(ptr_to_data);
     }
 
-    static auto alloc(size_t capacity) -> SmallVectorStorage<T>* {
+    static auto alloc(size_t capacity) -> storage<T>* {
         // only void* is allowed to be converted to uintptr_t
-        void* ptr = ::operator new(offsetToData + sizeof(T) * capacity);
+        void* ptr = ::operator new(offset_to_data + sizeof(T) * capacity);
 
         // make sure the lowest significant bit is 0
         auto intPtr = reinterpret_cast<std::uintptr_t>(ptr);
@@ -62,7 +62,7 @@ struct SmallVectorStorage : public SmallVectorHeader {
             throw std::bad_alloc(); // TODO use a better fitting exception?
         }
 
-        return new (ptr) SmallVectorStorage<T>(capacity);
+        return new (ptr) storage<T>(capacity);
     }
 };
 
@@ -79,7 +79,7 @@ class svector {
     };
     union {
         Direct m_direct;
-        detail::SmallVectorStorage<T>* m_indirect;
+        detail::storage<T>* m_indirect;
     } m_union;
 
     [[nodiscard]] auto is_direct() const -> bool {
@@ -92,7 +92,7 @@ public:
     };
 
     auto reserve(size_t s) {
-        m_union.m_indirect = detail::SmallVectorStorage<T>::alloc(s);
+        m_union.m_indirect = detail::storage<T>::alloc(s);
     }
 
     [[nodiscard]] auto capacity() const -> size_t {
