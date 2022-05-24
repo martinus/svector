3e292ecc9e022176a0be7ee98e0eee9d5aa8d984
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2022-05-24 07:56:41 +0200

    Coverate 99.3%!

--- src/test/resize.cpp
+++ src/test/resize.cpp
@@ -7,5 +7,29 @@
 TEST_CASE("resize") {
     Counter counts;
     auto a = ankerl::svector<Counter::Obj, 3>();
-    // TODO check resize_after_reserve std::destroy
+    a.resize(100);
+    REQUIRE(a.size() == 100);
+    for (auto const& x : a) {
+        REQUIRE(x == Counter::Obj());
+    }
+
+    a.clear();
+    for (size_t i = 0; i < 20; ++i) {
+        a.emplace_back(i + 100, counts);
+    }
+
+    a.resize(10);
+    REQUIRE(a.size() == 10);
+    for (size_t i = 0; i < a.size(); ++i) {
+        REQUIRE(a[i].get() == i + 100);
+    }
+
+    a.resize(3);
+    REQUIRE(a.size() == 3);
+    for (size_t i = 0; i < a.size(); ++i) {
+        REQUIRE(a[i].get() == i + 100);
+    }
+
+    a.resize(0);
+    REQUIRE(a.empty());
 }
