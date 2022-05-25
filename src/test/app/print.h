b8ca3c64937131e2780d38db9e1effa38ed63f10
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2022-05-25 08:58:39 +0200

    put print into a namespace, otherwise it clashes when fmt::join is used

--- src/test/app/print.h
+++ src/test/app/print.h
@@ -2,8 +2,12 @@
 
 #include <cstdio>
 
+namespace test {
+
 template <typename... Args>
 constexpr void print(fmt::format_string<Args...> f, Args&&... args) {
     fmt::print(f, std::forward<Args>(args)...);
     std::fflush(stdout);
 }
+
+} // namespace test
