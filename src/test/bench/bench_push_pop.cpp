35a3bcaf714d6a23fe12d129ba04454bab37d3d2
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2022-05-28 09:40:00 +0200

    better push_back benchmark

--- src/test/bench/bench_push_pop.cpp
+++ src/test/bench/bench_push_pop.cpp
@@ -1,33 +1,42 @@
 #include <ankerl/svector.h>
+#include <app/name_of_type.h>
+
+#include <doctest.h>
+#include <nanobench.h>
 
 #include <absl/container/inlined_vector.h>
 #include <boost/container/small_vector.hpp>
-#include <doctest.h>
-#include <nanobench.h>
 
 using namespace std::literals;
 
 template <typename Vec>
-void benchPushBack(char const* title, size_t num_elements) {
-    ankerl::nanobench::Bench().batch(num_elements).minEpochTime(100ms).run(title, [&] {
+void benchPushBack(size_t num_iters) {
+    auto title = name_of_type<Vec>();
+
+    // determine exact number of push_backs performed
+    auto rng = ankerl::nanobench::Rng(123);
+    auto num_push = size_t();
+    for (size_t i = 0; i < num_iters; ++i) {
+        num_push += rng.bounded(16);
+    }
+
+    ankerl::nanobench::Bench().batch(num_push).minEpochTime(100ms).run(std::string(title), [&] {
         auto rng = ankerl::nanobench::Rng(123);
-        for (size_t i = 0; i < 1000; ++i) {
+        for (size_t i = 0; i < num_iters; ++i) {
             auto vec = Vec();
-            auto num_push = rng.bounded(31);
+            auto num_push = rng.bounded(16);
             for (size_t i = 0; i < num_push; ++i) {
                 vec.push_back(i);
             }
-
-            while (!vec.empty()) {
-                vec.pop_back();
-            }
+            ankerl::nanobench::doNotOptimizeAway(vec.data());
         }
     });
 }
 
 TEST_CASE("push_back" * doctest::skip() * doctest::test_suite("bench")) {
-    benchPushBack<ankerl::svector<uint8_t, 7>>("svector<uint8_t, 31>", 1000);
-    benchPushBack<std::vector<uint8_t>>("std::vector<uint8_t>", 1000);
-    benchPushBack<absl::InlinedVector<uint8_t, 7>>("absl::InlinedVector<uint8_t, 31>", 1000);
-    benchPushBack<boost::container::small_vector<uint8_t, 7>>("boost::container::small_vector<uint8_t, 31>", 1000);
+    auto num_iters = 1000;
+    benchPushBack<ankerl::svector<uint8_t, 7>>(num_iters);
+    benchPushBack<std::vector<uint8_t>>(num_iters);
+    benchPushBack<absl::InlinedVector<uint8_t, 7>>(num_iters);
+    benchPushBack<boost::container::small_vector<uint8_t, 7>>(num_iters);
 }
