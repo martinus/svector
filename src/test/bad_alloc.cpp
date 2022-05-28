81865ef7481f369e7c16c3e4f264c7a5d4d6b6a2
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2022-05-28 08:27:40 +0200

    make build work with sanitizers

--- src/test/bad_alloc.cpp
+++ src/test/bad_alloc.cpp
@@ -3,19 +3,27 @@
 #include <valgrind/valgrind.h>
 
 #include <doctest.h>
+
 #include <limits>
 #include <stdexcept>
+#include <vector>
 
-TEST_CASE("reserve_bad_al") {
-    if (RUNNING_ON_VALGRIND) {
-        // this test doesn't work with valgrind:
-        //
-        // new/new[] failed and should throw an exception, but Valgrind
-        // cannot throw exceptions and so is aborting instead.  Sorry.doesn't work
+#define SANITIZER_ACTIVE 0
+
+#if defined(__has_feature)
+#    if __has_feature(address_sanitizer) || __has_feature(thread_sanitizer)
+#        undef SANITIZER_ACTIVE
+#        define SANITIZER_ACTIVE 1
+#    endif
+#endif
+
+TEST_CASE("reserve_bad_alloc") {
+    if (RUNNING_ON_VALGRIND || SANITIZER_ACTIVE) {
+        // this test doesn't work with valgrind or some sanitizers.
         return;
     }
     auto sv = ankerl::svector<std::string, 3>();
     auto m = sv.max_size();
-    REQUIRE(m == std::numeric_limits<size_t>::max());
-    REQUIRE_THROWS_AS(sv.reserve(m), std::bad_alloc);
+    REQUIRE(m == std::numeric_limits<std::ptrdiff_t>::max());
+    REQUIRE_THROWS_AS(sv.reserve(sv.max_size()), std::bad_alloc);
 }
