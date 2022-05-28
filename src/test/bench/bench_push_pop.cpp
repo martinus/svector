fafa61bd87e76a231e48b2e8d38e6f20597942ce
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2022-05-28 09:59:33 +0200

    add benchmarks

--- src/test/bench/bench_push_pop.cpp
+++ src/test/bench/bench_push_pop.cpp
@@ -6,6 +6,7 @@
 
 #include <absl/container/inlined_vector.h>
 #include <boost/container/small_vector.hpp>
+#include <fmt/format.h>
 
 using namespace std::literals;
 
@@ -39,4 +40,32 @@ TEST_CASE("push_back" * doctest::skip() * doctest::test_suite("bench")) {
     benchPushBack<std::vector<uint8_t>>(num_iters);
     benchPushBack<absl::InlinedVector<uint8_t, 7>>(num_iters);
     benchPushBack<boost::container::small_vector<uint8_t, 7>>(num_iters);
+}
+
+template <typename Vec, typename... Args>
+void bench_fill_emplace_back(size_t num_items, Args&&... args) {
+    auto title = fmt::format("fill_emplace_back {}", name_of_type<Vec>());
+
+    ankerl::nanobench::Bench().batch(num_items).minEpochTime(100ms).run(title, [&] {
+        auto vec = Vec();
+        for (size_t i = 0; i < num_items; ++i) {
+            vec.emplace_back(std::forward<Args>(args)...);
+        }
+        ankerl::nanobench::doNotOptimizeAway(vec.data());
+    });
+}
+
+// https://github.com/wichtounet/articles/blob/master/src/vector_list/bench.cpp
+TEST_CASE("fill_back" * doctest::skip() * doctest::test_suite("bench")) {
+    auto num_items = 1000000;
+    bench_fill_emplace_back<ankerl::svector<int, 7>>(num_items);
+    bench_fill_emplace_back<std::vector<int>>(num_items);
+    bench_fill_emplace_back<absl::InlinedVector<int, 7>>(num_items);
+    bench_fill_emplace_back<boost::container::small_vector<int, 7>>(num_items);
+
+    num_items = 1000000;
+    bench_fill_emplace_back<ankerl::svector<std::string, 7>>(num_items, "hello");
+    bench_fill_emplace_back<std::vector<std::string>>(num_items, "hello");
+    bench_fill_emplace_back<absl::InlinedVector<std::string, 7>>(num_items, "hello");
+    bench_fill_emplace_back<boost::container::small_vector<std::string, 7>>(num_items, "hello");
 }
