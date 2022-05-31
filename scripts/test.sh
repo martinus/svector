c241fcfe675535488e3b2808835dbfe3347355e1
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2022-05-31 05:25:37 +0200

    show number of builds

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,8 +4,12 @@
 set -e
 cd `git rev-parse --show-toplevel`
 
+c=0
 for D in build/*/ ; do
     echo
     echo "-- building $D --"
     meson test -q --print-errorlogs -C $D
+    ((c = c + 1))
 done
+echo
+echo "${c} builds ok"
